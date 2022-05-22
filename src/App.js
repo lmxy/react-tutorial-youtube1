5c35e020e87c0cec0c1222cc5442076c8f8dca53
Administrator <wjzxyaowei@gmail.com>
2022-05-22 22:53:32 +0800

    installed uuid
    save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState(
+    [{id: 1, name: "Todo1", completed: false}]);
+
+    const todoNameRef = useRef();
+
+    const handleAddTodo = () => {
+      // add task.
+      const name = todoNameRef.current.value;
+      setTodos((prevTodos) => {
+        return [...prevTodos, { id: uuidv4(), name: name, completed: false }]
+      });
+      todoNameRef.current.value = null;
+    }
 
   return(
     <div>
-      <TodoList />
-      <input type="text" />
-      <button>Add task</button>
+      <TodoList todos={todos} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>Add task</button>
       <button>Delete completed task</button>
       <div>Remained task:0</div>
     </div>
